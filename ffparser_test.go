b7de750a5d300f4a03e1823eb8f09f71c6bc074c
Emerson Henrique Soares Silva <emerson.hss@gmail.com>
2020-03-12 02:00:23 -0300

    Incluindo mais um teste.

--- ffparser_test.go
+++ ffparser_test.go
@@ -22,9 +22,10 @@ type TestPojo2 struct {
 }
 
 type TestDecorator1 struct {
-	NumInt     int     `record:"start=1,end=5,decorator=IntDecorator"`
-	NumInt64   int64   `record:"start=6,end=10,decorator=Int64Decorator"`
-	NumFloat64 float64 `record:"start=11,end=25,decorator=BrazilMoneyDecorator"`
+	NumInt       int     `record:"start=1,end=5,decorator=IntDecorator"`
+	NumInt64     int64   `record:"start=6,end=10,decorator=Int64Decorator"`
+	NumFloat64   float64 `record:"start=11,end=25,decorator=BrazilMoneyDecorator"`
+	NumFloat64_2 float64 `record:"start=26,end=40,decorator=BrazilMoneyDecorator"`
 }
 
 func TestShouldParseTextToStructWithAutoDetectFieldType(t *testing.T) {
@@ -53,10 +54,11 @@ func TestShouldParseTextToStructWithAutoDetectFieldTypeAndDecorators(t *testing.
 	parser := NewSimpleParser()
 
 	result, _ := parser.ParseToText(&TestDecorator1{
-		NumInt:     1,
-		NumInt64:   int64(12402),
-		NumFloat64: 178.5,
+		NumInt:       1,
+		NumInt64:     int64(12402),
+		NumFloat64:   178.5,
+		NumFloat64_2: 167.0,
 	})
 
-	assert.Equal(t, "0000112402000000000017850", result)
+	assert.Equal(t, "0000112402000000000017850000000000016700", result)
 }
